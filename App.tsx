9833cbe5dc30132af31a29b4f2d384f2e6cc46d6
ashishbhor <bhorv590@gmail.com>
2025-12-28 19:43:03 +0530

    Update app.tsx file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { RootStackParamList } from "./src/types";
+
+import SplashScreen from "./src/screens/SplashScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import ClientDetailsScreen from "./src/screens/ClientDetailsScreen";
+import EventSelectionScreen from "./src/screens/EventSelectionScreen";
+import ServicesScreen from "./src/screens/ServicesScreen";
+import PreviewScreen from "./src/screens/PreviewScreen";
+import HistoryScreen from "./src/screens/HistoryScreen";
+import { InvoiceProvider } from "./src/context/InvoiceContext";
+import 'react-native-gesture-handler';
+import 'react-native-screens';
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <InvoiceProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="ClientDetails" component={ClientDetailsScreen} />
+          <Stack.Screen name="EventSelection" component={EventSelectionScreen} />
+          <Stack.Screen name="Services" component={ServicesScreen} />
+          <Stack.Screen name="Preview" component={PreviewScreen} />
+          <Stack.Screen name="History" component={HistoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </InvoiceProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
